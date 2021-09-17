5db7948bcba3da5ae6bd63769c11b71c1502d838
NarminGale <narmingale@gmail.com>
2021-09-17 20:50:39 +0400

    Added logo to student-image div

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -5,9 +5,10 @@ function Student(props) {
   return (
     <>
       <article>
-        <div className="student-image">
+        <a href="#" className="student-image">
           <img src={props.img} />
-        </div>
+          <img src="./elshad-agayevs-school-logo.png" className="image-logo" />
+        </a>
         <div className="student-info">
           <h4 className="student-name">{props.name}</h4>
           <span className="student-job">{props.job}</span>
