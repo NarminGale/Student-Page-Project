29160d8b1147e8d91a8fcf15eff76e32d7400216
ali-rajabli <ali.recebli9@gmail.com>
2021-09-18 14:19:31 +0400

    new commit

--- src/pages/StudentDetails.js
+++ src/pages/StudentDetails.js
@@ -1,33 +1,36 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import students from '../data'
-import './StudentDetails.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ProfileAbout from "../components/profile-about/ProfileAbout";
+import ProfileHead from "../components/profile-head/ProfileHead";
+import students from "../data";
+import "./StudentDetails.css";
 
 function StudentDetails() {
-  const { id } = useParams()
-  const [name, setName] = useState('default name')
-  const [job, setJob] = useState('')
-  const [image, setImage] = useState(null)
-  const [about, setAbout] = useState('')
+  const { id } = useParams();
+  const [name, setName] = useState("default name");
+  const [job, setJob] = useState("");
+  const [image, setImage] = useState(null);
+  const [about, setAbout] = useState("");
 
   useEffect(() => {
-    const newPerson = students.find((student) => student.id === parseInt(id))
-    console.log(id)
+    const newPerson = students.find((student) => student.id === parseInt(id));
+    console.log(id);
     // console.log(student.id)
-    setName(newPerson.name)
-    setJob(newPerson.job)
-    setImage(newPerson.image)
-    setAbout(newPerson.text)
-  }, [])
+    setName(newPerson.name);
+    setJob(newPerson.job);
+    setImage(newPerson.image);
+    setAbout(newPerson.text);
+  }, []);
 
   return (
-    <>
-      <section className="container">
-        <h1>{name}</h1>
-      </section>
-    </>
-  )
+    <div className="student-details">
+      <div className="container">
+        <ProfileHead />
+        <ProfileAbout />
+      </div>
+    </div>
+  );
 }
 
-export default StudentDetails
+export default StudentDetails;
