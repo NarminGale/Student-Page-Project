1d66f21578f83b07b9e2b3bd7d1e72b81134d006
NarminGale <narmingale@gmail.com>
2021-09-18 11:52:52 +0400

    Imported css

--- src/pages/StudentDetails.js
+++ src/pages/StudentDetails.js
@@ -2,21 +2,31 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import students from '../data'
+import './StudentDetails.css'
 
 function StudentDetails() {
   const { id } = useParams()
   const [name, setName] = useState('default name')
+  const [job, setJob] = useState('')
+  const [image, setImage] = useState(null)
+  const [about, setAbout] = useState('')
 
   useEffect(() => {
     const newPerson = students.find((student) => student.id === parseInt(id))
     console.log(id)
     // console.log(student.id)
     setName(newPerson.name)
+    setJob(newPerson.job)
+    setImage(newPerson.image)
+    setAbout(newPerson.text)
   }, [])
+
   return (
-    <div>
-      <h1>{name}</h1>
-    </div>
+    <>
+      <section className="container">
+        <h1>{name}</h1>
+      </section>
+    </>
   )
 }
 
